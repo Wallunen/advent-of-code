9d2684992a44aaf02d937052932af9a7d5aa9f47
Wallunen <44848747+Wallunen@users.noreply.github.com>
2022-12-07 15:06:29 +0200

    Solve both parts of day 07 from 2022

---
+++ years/2022/days/07/1.ts
@@ -0,0 +1,49 @@
+import getInput from '/get-input.ts';
+
+let isOutput = false;
+const currentPath: string[] = [];
+const directorySizes = new Map<string, number>();
+
+for (const line of getInput(import.meta).split('\n')) {
+	if (line.startsWith('$ c')) {
+		isOutput = false;
+
+		const directory = line.slice(5);
+
+		switch (directory) {
+			case '/':
+				currentPath.length = 0;
+				break;
+			case '..':
+				--currentPath.length;
+				break;
+			default:
+				currentPath.push(directory);
+		}
+	} else if (line.startsWith('$ l')) {
+		isOutput = true;
+	} else if (isOutput) {
+		const fileSize = line.slice(0, line.indexOf(' '));
+
+		if (fileSize && fileSize !== 'dir') {
+			const parsedSize = Number.parseInt(fileSize, 10);
+
+			for (let i = 1; i <= currentPath.length; ++i) {
+				const joinedPath = currentPath.slice(0, i).join('/');
+				const previousSize = directorySizes.get(joinedPath) ?? 0;
+
+				directorySizes.set(joinedPath, previousSize + parsedSize);
+			}
+		}
+	}
+}
+
+let sumOfDirectorySizes = 0;
+
+for (const directorySize of directorySizes.values()) {
+	if (directorySize <= 100_000) {
+		sumOfDirectorySizes += directorySize;
+	}
+}
+
+console.log(sumOfDirectorySizes);

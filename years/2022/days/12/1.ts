c2eb063aee4585332698ac4b577d7836c74036c0
Wallunen <44848747+Wallunen@users.noreply.github.com>
2022-12-12 18:42:05 +0200

    Solve both parts of day 12 from 2022

---
+++ years/2022/days/12/1.ts
@@ -0,0 +1,72 @@
+import getInput from '/get-input.ts';
+
+const directions = [
+	[0, -1],
+	[0, +1],
+	[-1, 0],
+	[+1, 0],
+] as const;
+
+let end: number[] = [];
+let start: number[] = [];
+
+const grid = getInput(import.meta)
+	.split('\n')
+	.map((line, i) =>
+		line.split('').map((character, j) => {
+			let elevation = 0;
+
+			switch (character) {
+				case 'S':
+					start = [i, j];
+					break;
+				case 'E':
+					end = [i, j];
+					elevation = 25;
+					break;
+				default:
+					elevation = character.codePointAt(0)! - 97;
+			}
+
+			return elevation;
+		}),
+	);
+
+const seen: boolean[][] = [];
+const queue = [
+	{
+		stepCount: 0,
+		position: start,
+	},
+];
+
+while (queue[0].position[0] !== end[0] || queue[0].position[1] !== end[1]) {
+	const {
+		stepCount,
+		position: [row, column],
+	} = queue.shift()!;
+
+	if (seen[row]?.at(column)) {
+		continue;
+	}
+
+	for (const [rowAmount, columnAmount] of directions) {
+		const directionColumn = grid[row + rowAmount]?.at(column + columnAmount);
+
+		if (
+			directionColumn &&
+			directionColumn - 1 <= grid[row][column] &&
+			!seen[row + rowAmount]?.at(column + columnAmount)
+		) {
+			queue.push({
+				stepCount: stepCount + 1,
+				position: [row + rowAmount, column + columnAmount],
+			});
+		}
+	}
+
+	seen[row] ??= [];
+	seen[row][column] = true;
+}
+
+console.log(queue[0].stepCount);
